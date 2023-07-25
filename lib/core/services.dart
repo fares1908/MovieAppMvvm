25f50cb0b98707b03f3da6ac5c2ca8312c8ef49f
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-25 18:34:13 +0300

    ;

---
+++ lib/core/services.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+class ApiServices{
+  ApiServices(this.dio);
+  static const String baseUrl='https://api.themoviedb.org/3/movie/';
+  static const String apiKey="6ef7ef2de9abba7f43f417733b4310a1";
+  final Dio dio;
+
+  Future<Map<String,dynamic>> getMovies({
+    required String endPoint,
+}) async{
+    var response=await dio.get('$baseUrl$endPoint?api_key=$apiKey');
+    return response.data;
+    
+  }
+}
