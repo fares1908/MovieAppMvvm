3ec197dff651f42c63b5f1159290b2db52370614
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-25 18:33:16 +0300

    ;

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:cars/core/utils/Router.dart';
+import 'package:cars/features/Home/data/repositories/home_repo_imple.dart';
 import 'package:cars/features/Home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc_observer.dart';
+import 'core/utils/service_locator.dart';
+import 'features/Home/presentation/manager/movie_bloc.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setup();
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
@@ -12,11 +21,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-     routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+     BlocProvider(create: (context) =>MovieBloc(
+        getIt.get<HomeRepoImpl>()..fetchPopularMovies()
+     )
+     )
+      ],
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+       routerConfig: AppRouter.router,
+      ),
     );
   }
 }
