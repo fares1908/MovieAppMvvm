61b495c30b38c00cd00fc3d0e42a6f61772991ba
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-26 15:34:00 +0300

    top rated movies

--- lib/features/Home/presentation/views/widgets/TopRatedMovies.dart
+++ lib/features/Home/presentation/views/widgets/TopRatedMovies.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assetsdata.dart';
 import 'ListViewItem.dart';
+import 'Top_rated_movies_body.dart';
 
 class TopRatedMovies extends StatelessWidget {
   const TopRatedMovies({super.key});
@@ -9,14 +10,10 @@ class TopRatedMovies extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('TopRated Movies'),
-        ),
-        body: ListView.builder(
-          itemBuilder: (context, index) => const ListViewItem(
-            imageUrl: '${AssetData.test}', textName: 'fares', releaseDate: '2020', vote: 3, descreption: 'dgsjyyyyyyyyy',),
-          itemCount: 16,
-        )
+      appBar: AppBar(
+        title: Text('TopRated Movies'),
+    ),
+      body: TopRatedMoviesBody(),
     );
   }
 }
---
+++ lib/features/Home/presentation/views/widgets/Top_rated_movies_body.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/utils/assetsdata.dart';
+import '../../../../../core/utils/widget/CustomErrorWidget.dart';
+import '../../manager/movie_bloc.dart';
+import 'ListViewItem.dart';
+
+class TopRatedMoviesBody extends StatelessWidget {
+  const TopRatedMoviesBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MovieBloc, MovieState>(
+      builder: (context, state) {
+        return BlocBuilder<MovieBloc, MovieState>(
+          builder: (context, state) {
+            if (state is TopRatedMovieSuccess) {
+              return Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height *.8,
+                    child: ListView.builder(
+                      itemCount:state.movies.length,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ListViewItem(
+
+                            imageUrl: state.movies[index].backdropPath,
+                            textName: state.movies[index].title,
+                            releaseDate: state.movies[index].releaseDate,
+                            vote:state.movies[index].voteAverage,
+                            descreption: state.movies[index].overview,
+
+            ),
+                    ),
+
+                  ),
+                  )
+                ],
+              );
+            } else if (state is TopRatedMovieError) {
+              return CustomErrorWidget(
+                message: state.errorMessage,
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        );
+      },
+    );
+  }
+}
