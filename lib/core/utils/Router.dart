eccf1437c0cbba112bfeb9becbf4fbec0a5f02cd
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-26 15:33:26 +0300

    popular

--- lib/core/utils/Router.dart
+++ lib/core/utils/Router.dart
@@ -1,33 +1,52 @@
 import 'package:cars/SplashScreen.dart';
+import 'package:cars/core/utils/service_locator.dart';
+import 'package:cars/features/Home/data/repositories/home_repo_imple.dart';
+import 'package:cars/features/Home/presentation/manager/movie_bloc.dart';
 import 'package:cars/features/Home/presentation/views/home_view.dart';
 import 'package:cars/features/Home/presentation/views/widgets/TopRatedMovies.dart';
 import 'package:cars/features/Home/presentation/views/widgets/popularMovies.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-abstract class AppRouter{
-
-static const String kHomeView='/homeView';
-static const String kTopRatedView='/topRatedView';
-static const String kPopularView='/popularView';
- static final router=GoRouter(
-     routes:[
-       GoRoute(
-           path:'/',
-         builder: (context, state) =>SplashScreen(),
-       ),
-       GoRoute(
-           path: AppRouter.kHomeView,
-         builder: (context, state) => const HomeView(),
-       ),
-       GoRoute(
-           path: AppRouter.kTopRatedView,
-         builder: (context, state) => const TopRatedMovies(),
-       ),
-        GoRoute(
-           path: AppRouter.kPopularView,
-         builder: (context, state) => const PopularMovies(),
-       ),
-     ]
- );
-
+abstract class AppRouter {
+  static const String kHomeView = '/homeView';
+  static const String kTopRatedView = '/topRatedView';
+  static const String kPopularView = '/popularView';
+  static final router = GoRouter(routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => SplashScreen(),
+    ),
+    GoRoute(
+      path: AppRouter.kHomeView,
+      builder: (context, state) =>
+          BlocProvider(
+            create: (context) {
+              return MovieBloc(getIt.get<HomeRepoImpl>())
+                ..
+                add(LoadNowPlayingMovies())..add(LoadPopularMovies())..add(
+                    LoadTopRatedMovies());
+            },
+            child: const HomeView(),
+          ),
+    ),
+    GoRoute(
+      path: AppRouter.kTopRatedView,
+      builder: (context, state) =>
+          BlocProvider(
+            create: (context){
+              return MovieBloc(getIt.get<HomeRepoImpl>())..add(LoadTopRatedMovies());
+            },
+            child: const TopRatedMovies(),
+          ),
+    ),
+    GoRoute(
+      path: AppRouter.kPopularView,
+      builder: (context, state) => BlocProvider(
+          create: ( context) {
+            return MovieBloc(getIt.get<HomeRepoImpl>())..add(LoadPopularMovies());
+          },
+          child: const PopularMovies()),
+    ),
+  ]);
 }
--- lib/features/Home/presentation/views/widgets/popularSecation.dart
+++ lib/features/Home/presentation/views/widgets/popularSecation.dart
@@ -1,52 +1,36 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cars/core/utils/Router.dart';
+import 'package:cars/features/Home/data/models/movie.dart';
 import 'package:cars/features/Home/presentation/manager/movie_bloc.dart';
+import 'package:cars/features/Home/presentation/views/widgets/popular_item_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assetsdata.dart';
 import '../../../../../core/utils/widget/CustomErrorWidget.dart';
 import 'SeeMore.dart';
 
 class PopularSection extends StatelessWidget {
-  const PopularSection({super.key});
+  const PopularSection({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MovieBloc, MovieState>(builder: (context, state) {
-      if (state is PopularMovieSuccess) {
-        return Column(
-          children: [
-            SeeMore(text: ' Popular', location: AppRouter.kPopularView),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * .3,
-              child: ListView.builder(
-                itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    // child: ClipRRect(
-                    //   borderRadius: BorderRadius.circular(12),
-                    //   child: CachedNetworkImage(
-                    //       imageUrl: state.movies[index]),
-                    // )
-                  child: Text(
-                    state.movies[index].releaseDate
-                  ),
 
-                )
-                ,
-                itemCount: 5,
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-          ],
-        );
-      }
-      else if (state is PopularMovieError){
-          return CustomErrorWidget(message: state.errorMessage,);
-      }
-      else {
-        return const Center(child: CircularProgressIndicator());
-      }
-    });
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            GoRouter.of(context).push(AppRouter.kPopularView);
+          },
+            child: SeeMore(
+          text: ' Popular',
+        )),
+        SizedBox(height: 10),
+        PopularItemsListView()
+      ],
+    );
   }
 }
